7e57a903e0c0e465cff7c77744385193d37cb913
Mickael Brunet <mickael.brunet64@gmail.com>
2022-04-04 20:23:10 +0200

    Avancement page about

--- container/AboutPage/AboutPage.js
+++ container/AboutPage/AboutPage.js
@@ -1,11 +1,57 @@
 import React from 'react'
-import { AboutPageWrapper, Title } from './AboutPage.style'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AboutPageWrapper, SubTitle, Title, SideText, Section, ImageWrapper } from './AboutPage.style'
+import AboutUser from '../../public/images/AboutUser.png'
 
-const AboutPage = ({ pageData }) => {
+const AboutPage = ({ pageData }) => {
   return (
+    <>
     <AboutPageWrapper>
+
+      <Section>
         <Title> About </Title>
+        <SubTitle> L'endroit ou vous réaliserez vos plus grand rêves. </SubTitle>
+      </Section>
+
+      <Section>
+        <SideText>Qui sommes-nous ?</SideText>
+        <Title> Société de location de matériels audiovisuels </Title>
+        <ImageWrapper>
+          <Image src={AboutUser}></Image>
+        </ImageWrapper>
+      </Section>
+
+        <Section>
+          <SideText>Agence composée de 3 membres</SideText>
+          <Title>Jessica Ferreira - Raphael Jeune - Lucie Marques</Title>
+          <ImageWrapper>
+            <Image src={AboutUser}></Image>
+          </ImageWrapper>
+        </Section>
+
+        <Section>
+          <SideText>Work</SideText>
+          <ImageWrapper>
+            <Link href={'#'}>
+              <Image src={AboutUser}></Image>
+            </Link>
+            <Link href={'#'}>
+              <Image src={AboutUser}></Image>
+            </Link>
+            <Link href={'#'}>
+              <Image src={AboutUser}></Image>
+            </Link>
+          </ImageWrapper>
+        </Section>
+
+        <Section>
+          <SideText>Avis</SideText>
+          
+        </Section>
+
     </AboutPageWrapper>
+    </>
   )
 }
 
--- container/AboutPage/AboutPage.style.js
+++ container/AboutPage/AboutPage.style.js
@@ -3,14 +3,55 @@ import styled from "styled-components";
 export const AboutPageWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
+    background-color: black;
+`;
+
+export const Section = styled.section`
+    width: 100%;
+    height: 100%;
+`;
+
+export const Return = styled.p`
+    height: 20px;
+
 `;
 
 export const Title = styled.h1`
-    font-size: 3rem;
-    color: black;
-    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4rem;
+    color: white;
     margin-bottom: 2rem;
+    font-weight: bold;
+    margin-top: 200px;
 `;
+
+export const SubTitle = styled.h2`
+    display: flex;
+    justify-content: center;
+    font-size: 2rem;
+    color: white;
+    margin-bottom: 10rem;
+    opacity : 0.5
+    background-color: yellow;
+`
+
+export const SideText = styled.h4`
+    display: flex;
+    font-size: 1.5rem;
+    color: white;
+    text-align: left;
+    margin-bottom: 2rem;
+    margin-left: 50px;
+`
+
+export const ImageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+
+`;
+
