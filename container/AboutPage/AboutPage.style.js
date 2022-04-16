eed74731848de166f800621f3d5ffc3be9188235
Mickael Brunet <mickael.brunet64@gmail.com>
2022-04-16 21:55:29 +0200

    Ajout de media query pour le device smartphones

--- container/AboutPage/AboutPage.style.js
+++ container/AboutPage/AboutPage.style.js
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
+const size = {
+    mobileSizeStandard : '667px',
+    mobileSizePlus: '736px',
+    tabletSizeMini : '768px',
+    tabletSizeStandard : '768px',
+    desktopSize: '1200',
+};
+
+export const deviceLandscape = {
+    mobileSizeStandard: `(min-width : ${size.mobileSizeStandard})`,
+    mobileSizePlus: `(min-width : ${size.mobileSizePlus})`,
+    tabletSizeMini: `(min-width : ${size.tabletSizeMini})`,
+    tabletSizeStandard: `(min-width : ${size.tabletSizeStandard})`,
+    desktopSize: `(min-width : ${size.desktopSize})`
+}
+
+export const devicePortrait = {
+    mobileSizeStandard: `(max-width : ${size.mobileSizeStandard})`,
+    mobileSizePlus: `(min-width : ${size.mobileSizePlus})`,
+    tabletSizeMini: `(min-width : ${size.tabletSizeMini})`,
+    tabletSizeStandard: `(min-width : ${size.tabletSizeStandard})`,
+    desktopSize: `(min-width : ${size.desktopSize})`
+}
+
 export const AboutPageWrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -11,6 +35,11 @@ export const Section = styled.section`
     height: 100%;
     margin-bottom: 50px;
     margin-top: 130px;
+
+    @media only screen and (max-width: 991px) {
+    
+        
+  }
 `;
 
 export const Return = styled.p`
@@ -27,6 +56,32 @@ export const Title = styled.h1`
     margin-bottom: 2rem;
     font-weight: bold;
     margin-top: 100px;
+
+    @media ${deviceLandscape.mobileSizeStandard} {
+        font-size: ${props => '3rem'};
+        text-align: center;
+    }
+
+    @media ${deviceLandscape.mobileSizePlus} {
+        font-size: ${props => '2rem'};
+    }
+
+    @media ${devicePortrait.mobileSizeStandard} {
+        font-size: ${props => '2rem'};
+        text-align: center;
+    }
+
+    @media ${devicePortrait.tabletSizeMini} {
+        font-size: ${props => '4rem'};
+    }
+
+    @media ${deviceLandscape.tabletSizeMini} {
+        font-size: ${props => '3rem'};
+    }
+
+    @media ${devicePortrait.tabletSizeStandard} {
+        
+    }
 `;
 
 export const SubTitle = styled.h2`
@@ -36,6 +91,20 @@ export const SubTitle = styled.h2`
     color: white;
     margin-bottom: 10rem;
     opacity : 0.5;
+
+    @media ${deviceLandscape.mobileSizeStandard} {
+        text-align: center;
+    }
+
+    @media ${devicePortrait.mobileSizeStandard} {
+        text-align: center;
+        font-size: 1.4rem;
+    }
+
+    @media ${devicePortrait.tabletSizeMini} {
+        text-align: center;
+        font-size: 1.4rem;
+    }
 `
 
 export const SideText = styled.h4`
@@ -45,6 +114,10 @@ export const SideText = styled.h4`
     text-align: left;
     margin-bottom: 2rem;
     margin-left: 50px;
+
+    @media ${devicePortrait.mobileSizeStandard} {
+        font-size: 1.2rem;
+    }
 `
 
 export const ImageWrapper = styled.div`
@@ -58,6 +131,14 @@ export const CommentContent = styled.p`
     color: white;
     font-size: 25px;
     margin-left: 50px;
+
+    @media ${devicePortrait.mobileSizeStandard} {
+        font-size: 10px;
+    }
+
+    @media ${deviceLandscape.mobileSizePlus} {
+        font-size: 12px;
+    }
 `;
 
 export const CommentWrapper = styled.section`
